999951d144efd6459febbb64b82a6990b62ce7a5
Sigur Gouwens <sigur.gouwens@rivm.nl>
2023-12-14 22:28:07 +0100

    New features in the convolution class. Mainly distribution properties, but also rescaling the domain of the distributions

--- distmaker.py
+++ distmaker.py
@@ -5,22 +5,22 @@ import seaborn as sns
 from scipy.stats import norm, binom, gaussian_kde
 from scipy.fft import fft, ifft
 from scipy.interpolate import interp1d
+from scipy.integrate import simps
+from scipy.optimize import bisect
 from scipy.signal import fftconvolve
 
 import matplotlib.pyplot as plt
 
 p = 0.1
-N_pop = 100000
+N_pop = 250
 N_cost = int(np.ceil(N_pop*p))
 
 price = np.random.normal(loc=250, scale=10, size=N_cost)
 amount = np.random.negative_binomial(9, 0.85, N_cost)
 costs = price*amount
+costs = costs
 
 class Distributionfinder:
-    
-    costs_mean = np.mean(costs)
-    costs_sd= np.std(costs)    
     
     def __init__(self, data, popsize, prob, density_bootstrap=None, dist_clt_trick=None):
         self.data = data
@@ -57,8 +57,8 @@ class Distributionfinder:
     def generate_dist_clt_value(self, z):
         """"""
         
-        costs_mean = np.mean(costs)
-        costs_sd = np.std(costs)  
+        costs_mean = np.mean(self.data)
+        costs_sd = np.std(self.data)  
         
         binom_mean = self.popsize * self.prob
         binom_sd = np.sqrt(self.popsize * self.prob * (1-self.prob))
@@ -91,8 +91,10 @@ class Convolver():
     This class should not be used at this point"""
     
     def __init__(self, x, y):
+        # make sure can only be loaded if dist is non-empty
         self.x = x
         self.y = y
+        # Here, apply some useful functions beforehand.
     
     def __add__(self, other):
         pass
@@ -100,33 +102,82 @@ class Convolver():
         convolve function which performs numerical integration. The accuracy in
         our use case is equal. However in general this may not be true."""
         
-        common_grid = np.linspace(min(self.x.min(), other.x.min()), max(self.x.max(), other.x.max()), 300)
+        common_grid = np.linspace(self.x.min()-np.abs(other.x.min()), self.x.max()+other.x.max(), 3000)
         delta_x = common_grid[1] - common_grid[0]
         
         y1_on_common_grid = interp1d(self.x, self.y, kind='linear', fill_value=0, bounds_error=False)(common_grid)
         y2_on_common_grid = interp1d(other.x, other.y, kind='linear', fill_value=0, bounds_error=False)(common_grid)
-        
+        # Below does not work as it should yet
         convolution_result = fftconvolve(y1_on_common_grid, y2_on_common_grid, mode='same') * delta_x
         
-        return Convolver(common_grid, convolution_result)
+        new_distribution = Convolver(common_grid, convolution_result)
+        new_distribution.correct_boundaries()
+        return new_distribution
     
-    def get_quantiles(self, q1, q2):
-        # Use interpolation to do this accurately
-        pass
+    def plot_density(self):
+        sns.lineplot(x=self.x, y=self.y)
+    
+    def compute_moments(self, k):
+        """Computes the k'th moment of the distribution using numerical integration"""
+        return simps(self.x**k * self.y, self.x)
+    
+    def compute_probability(self, lower, upper): # Use the base of get_quantile
+        """Computes the integral int_lower^upper f(x) dx"""
+        minx = min(self.x)
+        maxx = max(self.x)
         
+        idx = (max(lower, minx) < self.x) & (min(upper, maxx) < self.x) # not correct
+        return simps(self.y[idx], self.x[idx])
+    
+    def get_sd(self):
+        return np.sqrt(self.compute_moments(2) - self.compute_moments(1)**2)
+            
+    def interpolate_pdf(self, value):
+        dx = self.x[1] - self.x[0]
+        interp_func = interp1d(self.x, self.y, kind='linear', fill_value='extrapolate')
+        return float(interp_func(value))
+    
+    def get_quantiles(self, val):
+        """WE NEED TO AGGREGATE HERE"""
+        dx = self.x[1] - self.x[0]        
+        cumsum_y = np.cumsum(self.y) * dx
+         
+        interp_func = interp1d(self.x, cumsum_y, kind='linear', fill_value='extrapolate')
+        return float(interp_func(val))
+    
+    def get_probability(self, val1, val2):
+        return self.get_quantiles(val2) - self.get_quantiles(val1)
+            
+    def get_ranges(self, bounds_at=1e-7, tol=1e-9):
+        """This function may fail when using an unimodal function or when the variance is very large."""
+        
+        mean = self.compute_moments(1)
+        sd = self.get_sd()
+        
+        def root_function(x):
+            return self.interpolate_pdf(x) - bounds_at
+        
+        root_left = bisect(root_function, mean - 10*sd, mean, xtol=tol)
+        root_right = bisect(root_function, mean + 10*sd, mean, xtol=tol)
+        return root_left, root_right
+        
+    def correct_boundaries(self):
+        """Iteratively search density function to find a value such that f is under 1e-6"""
+        lower, upper = self.get_ranges()
+        idx = (lower < self.x) & (self.x < upper)
+        self.x = self.x[idx]
+        self.y = self.y[idx]
+
 distf = Distributionfinder(data=costs, popsize=N_pop, prob=p)
+distf.generate_dist_clt_trick(00, 3e4, 1000) # grenzen hier automatisch maken adhv input
 
-# distf.generate_bootstrap_density(5000)
-# distf.plot_density(type='bootstrap')
+ct = Convolver(distf.density_clt[0], distf.density_clt[1])
+ct.correct_boundaries()
+ct.plot_density() 
 
-distf.generate_dist_clt_trick(3.72e6, 4.2e6, 500)
-#distf.plot_density(type='clt')
+#convtest.compute_probability(1e6, 8.1e6) # FOUT!
 
+ct.get_probability(5000, 15000)
 
-convtest = Convolver(distf.density_clt[0], distf.density_clt[1])
-
-plt.plot(convtest.x, convtest.y)
-
-convtest2 = convtest + convtest
-
-plt.plot(convtest2.x, convtest2.y)
+ctct = ct+ct
+ctct.plot_density()
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-pass
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from distmaker import Distributionfinder, Convolver
+
+from scipy.stats import norm
+# Implement actual use case where we have a whole population (N_pop varies over age, and p too)
+# Then use Convolver to convolute it together
+
+p = 0.1
+N_pop = 100000
+N_cost = int(np.ceil(N_pop*p))
+
+price = np.random.normal(loc=250, scale=10, size=N_cost)
+amount = np.random.negative_binomial(9, 0.85, N_cost)
+costs = price*amount
+costs = costs - np.mean(costs)
+
+distf = Distributionfinder(data=costs, popsize=N_pop, prob=p)
+
+# distf.generate_bootstrap_density(5000)
+# distf.plot_density(type='bootstrap')
+
+distf.generate_dist_clt_trick(-2e5, 2e5, 500)
+#distf.plot_density(type='clt')
+
+# Convolver zodanig aanpassen dat een element van Distfinder gewoon alles mooi laat lopen. geen lelijke dingen met [1]
+convtest = Convolver(distf.density_clt[0], distf.density_clt[1])
+convtest2 = convtest + convtest
+convtest2.plot_density()
+
+# Als convolueren altijd goed gaat zonder onnodig lange intervals
+# def prob_fun(x):
+#     return (0.3 - 0.027*x + 0.02*(x/5)**2 - 0.05*(x/20)**3) / 2
+
+# a_range = np.linspace(0,80, num=81)
+# p_range = prob_fun(a_range)
+
+# sns.lineplot(x=a_range, y=p_range)
